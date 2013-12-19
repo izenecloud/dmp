36077f056876affb20153c9f0db1101eb54baac2
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-12-19 11:56:21 +0800

    Using MaxEnt classifier in GetCategory UDF

--- src/main/java/com/b5m/pig/udf/GetCategory.java
+++ src/main/java/com/b5m/pig/udf/GetCategory.java
@@ -1,15 +1,28 @@
 package com.b5m.pig.udf;
 
-import java.io.IOException;
+import com.b5m.maxent.CategoryClassifier;
+import com.b5m.maxent.MaxEnt;
 
 import org.apache.pig.EvalFunc;
 import org.apache.pig.data.Tuple;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * UDF that maps a string to a product category.
  * @author Paolo D'Apice
  */
 public class GetCategory extends EvalFunc<String> {
+
+    private final String filename;
+    private CategoryClassifier classifier = null;
+
+    public GetCategory(String filename) {
+        this.filename = filename;
+    }
 
     @Override
     public String exec(Tuple tuple) throws IOException {
@@ -18,10 +31,23 @@ public class GetCategory extends EvalFunc<String> {
         }
 
         String title = (String) tuple.get(0);
-        String category = null;
-        // TODO code here
 
-        return category;
+        if (classifier == null) // TODO move to constructor?
+            classifier = new MaxEnt(new File(MaxEnt.MODEL_FILENAME));
+
+        return classifier.getCategory(title);
+    }
+
+    @Override
+    public List<String> getCacheFiles() {
+        String name = String.format("%s#%s", filename, MaxEnt.MODEL_FILENAME);
+        return Arrays.asList(name);
+    }
+
+    /* for tests only */
+    GetCategory(File file) throws IOException {
+        filename = file.toString();
+        classifier = new MaxEnt(file);
     }
 
 }
--- src/test/java/com/b5m/pig/udf/TestGetCategory.java
+++ src/test/java/com/b5m/pig/udf/TestGetCategory.java
@@ -7,25 +7,38 @@ import org.testng.annotations.Test;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 
 public class TestGetCategory {
 
-    private TupleFactory tupleFactory = TupleFactory.getInstance();
-    private GetCategory func = new GetCategory();
+    private final TupleFactory tupleFactory = TupleFactory.getInstance();
+    private final GetCategory func;
 
-    @DataProvider(name="tuples")
+    public TestGetCategory() throws IOException {
+        URL url =  getClass().getResource("/Model.txt");
+        File file = new File(url.getFile());
+        func = new GetCategory(file);
+    }
+
+    @DataProvider(name="titles")
     public Object[][] titles() {
         return new Object[][] {
-            { tupleFactory.newTuple("title"), null }, // TODO to be implemented
+            // input title, output category
+            { "蔻玲2013冬新款女狐狸毛领羊绒呢子短款大衣寇玲原价1999专柜正品", "服装服饰" },
+            { "深部条带煤柱长期稳定性基础实验研究 正版包邮", "图书音像" },
+            // TODO { "", "" },
+            // TODO { null, "" },
         };
     }
 
-    @Test(dataProvider="tuples")
-    public void test(Tuple tuple, String category) throws IOException {
+    @Test(dataProvider="titles")
+    public void test(String title, String category) throws IOException {
+        Tuple tuple = tupleFactory.newTuple(title);
         String output = func.exec(tuple);
-        assertEquals(category, output);
+        assertEquals(output, category);
     }
 
 }
