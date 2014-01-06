5e3df6ec4a186470396d0f40ea816bca31b2bafd
Paolo D'Apice <paolo.dapice@izenesoft.com>
2014-01-06 10:21:55 +0800

    Renamed test class

--- src/test/java/com/b5m/gson/TestGsonSerialization.java
+++ src/test/java/com/b5m/gson/TestGsonSerialization.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class TestJsonSerialization {
+public class TestGsonSerialization {
 
     private Gson gson = new Gson();
 
