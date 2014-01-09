46a287fd2ba77ebbb6a502c62b24c64c3e395e4f
Paolo D'Apice <paolo.dapice@izenesoft.com>
2014-01-09 11:46:47 +0800

    Updated SimpleDmp test case

--- src/test/java/com/b5m/pig/SimpleDmpOnPig.java
+++ src/test/java/com/b5m/pig/SimpleDmpOnPig.java
@@ -1,65 +1,36 @@
 package com.b5m.pig;
 
 import static org.testng.Assert.*;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.pig.pigunit.PigTest;
-import org.apache.pig.tools.parameters.ParseException;
-import com.couchbase.client.CouchbaseClient;
 
 import java.io.IOException;
 import java.io.File;
-import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class SimpleDmpOnPig {
 
-    private final static String BUCKET = "default";
-    private final static String PASSWORD = "";
-    private final static String HOST = "http://127.0.0.1:8091/pools";
-
-    private static CouchbaseClient client;
-    private static Set<String> uuids;
-
-    @BeforeClass
-    public static void connect() throws Exception {
-        List<URI> hosts = Arrays.asList(new URI(HOST));
-        client = new CouchbaseClient(hosts, BUCKET, PASSWORD);
-    }
+    private static String[] expected;
 
     @BeforeClass
     private static void getExpected() throws Exception {
-        List<String> lines = FileUtils.readLines(new File("src/test/data/uuid.txt"));
-        uuids = new TreeSet<String>(lines);
+        List<String> lines = FileUtils.readLines(new File("src/test/data/simple-dmp.output"));
+        expected = lines.toArray(new String[lines.size()]);
     }
+
     @Test
-    public void test() throws IOException, ParseException {
+    public void test() throws Exception {
         String[] args = {
-            "./src/test/pig/simple-dmp.properties"
+            "src/test/pig/simple-dmp.properties"
         };
 
-        PigTest test = new PigTest("./src/main/pig/simple-dmp.pig", null, args);
+        PigTest test = new PigTest("src/main/pig/simple-dmp.pig", null, args);
 
-        test.unoverride("STORE"); // enables write to couchbase
-        test.runScript();
-
-        for (String uuid : uuids) {
-            assertNotNull(client.get(uuid));
-        }
+        test.assertOutput("data5", expected);
     }
 
-    @AfterClass
-    public static void cleanup() throws Exception {
-        for (String uuid : uuids) {
-            assertTrue(client.delete(uuid).get());
-        }
-        client.shutdown();
-    }
 }
 
