dbcc9ef65b5b7c4b9f22e9c21e2a27ea4dee66f3
Paolo D'Apice <paolo.dapice@izenesoft.com>
2014-01-13 12:51:00 +0800

    Added test for running Pig script embedded into Java program

---
+++ src/test/java/com/b5m/pig/udf/GetCategoryEmbedded.java
@@ -0,0 +1,64 @@
+package com.b5m.pig.udf;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import org.apache.pig.PigServer;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.backend.executionengine.ExecJob;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GetCategoryEmbedded {
+
+    private PigServer pigServer = null;
+
+    @BeforeTest
+    public void setup() throws Exception {
+        pigServer = new PigServer("local");
+    }
+
+    @AfterTest
+    public void teardown() {
+        pigServer.shutdown();
+    }
+
+    @Test
+    public void runEmbedded() throws IOException {
+        pigServer.registerJar("dist/pig-udfs.jar");
+        pigServer.registerFunction("GET_CATEGORY",
+                new org.apache.pig.FuncSpec("com.b5m.pig.udf.GetCategory",
+                    new String[] { "src/test/resources/Model.txt", "local" }
+                    )
+                );
+        pigServer.registerQuery("titles = LOAD 'src/test/data/sample-logs.avro' AS (title:chararray);");
+        pigServer.registerQuery("categories = FOREACH titles GENERATE GET_CATEGORY(title);");
+
+        pigServer.deleteFile("output");
+        ExecJob job = pigServer.store("categories", "output");
+        assertEquals(job.getStatus(), ExecJob.JOB_STATUS.COMPLETED);
+    }
+
+    @Test
+    public void loadEmbedded() throws IOException {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("model_file", "src/test/resources/Model.txt");
+        params.put("input", "src/test/data/sample-logs.avro");
+
+        pigServer.deleteFile("output");
+        pigServer.registerScript("src/test/pig/getCategory.pig", params);
+
+        pigServer.setBatchOn();
+        List<ExecJob> jobs = pigServer.executeBatch();
+        assertEquals(jobs.size(), 1);
+        for (ExecJob job : jobs) {
+            assertEquals(job.getStatus(), ExecJob.JOB_STATUS.COMPLETED);
+        }
+    }
+}
+
--- src/test/pig/getCategory.pig
+++ src/test/pig/getCategory.pig
@@ -1,11 +1,13 @@
 -- register the jar file
 REGISTER ./dist/pig-udfs.jar
 
+%default input 'unused'
+
 -- alias function name
 DEFINE GET_CATEGORY com.b5m.pig.udf.GetCategory('$model_file', 'local');
 
 -- load input passed to this script
-titles = LOAD 'input' as (title:chararray);
+titles = LOAD '$input' as (title:chararray);
 
 -- process data
 categories = FOREACH titles GENERATE GET_CATEGORY(title);
