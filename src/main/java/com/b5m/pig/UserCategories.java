7d6a1eb1e0ddb531d6f5111759b421363309df85
Paolo D'Apice <paolo.dapice@izenesoft.com>
2014-01-22 10:52:32 +0800

    Embedded script for user categories on several dates

---
+++ src/main/java/com/b5m/pig/UserCategories.java
@@ -0,0 +1,97 @@
+package com.b5m.pig;
+
+import com.b5m.utils.DatesGenerator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.pig.FuncSpec;
+import org.apache.pig.PigServer;
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.backend.executionengine.ExecJob;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+public class UserCategories {
+
+    private final static Log log = LogFactory.getLog(UserCategories.class);
+
+    private String date;
+    private final int count;
+
+    private Properties properties = new Properties();
+    private PigServer pig = null;
+
+    public UserCategories(String date) {
+        this(date, 1);
+    }
+
+    public UserCategories(String date, int count) {
+        this.date = date;
+        this.count = count;
+    }
+
+    public void addPropertiesFile(String file)
+    throws FileNotFoundException, IOException {
+        properties.load(new FileInputStream(file));
+        if (log.isDebugEnabled()) log.debug("loaded properties: " + properties);
+    }
+
+    public ExecJob run() throws ExecException, IOException { // TODO implement Callable?
+        String mode = properties.getProperty("mode");
+        pig = new PigServer(mode);
+
+        DatesGenerator dategen = new DatesGenerator();
+        List<String> dates = dategen.getDates(date, count);
+
+        String basedir = properties.getProperty("basedir");
+        StringBuilder input = new StringBuilder();
+        for (String d : dates) {
+            input.append(basedir).append("/").append(d).append(",");
+        }
+        input.deleteCharAt(input.length() - 1);
+
+        pig.registerQuery("a = LOAD '" + input.toString() + "' USING JsonLoader();");
+
+        pig.registerFunction("Normalize", function("com.b5m.pig.udf.NormalizeMap"));
+        pig.registerFunction("Merge", function("com.b5m.pig.udf.MergeMaps"));
+
+        pig.registerQuery("b = GROUP a BY uuid;");
+        pig.registerQuery("c = FOREACH b {"
+        + "  m = Merge(a.categories);"
+        + "  n = Normalize(m);"
+        + "  GENERATE CONCAT(group, '::" + date + "') AS uuid, n AS categories; "
+        + "}");
+
+        // XXX there's a bug in PigServer so it won't use previously registered
+        //     functions in method store; as workaround directly use a string
+        FuncSpec storefunc = function("com.b5m.pig.udf.CouchbaseStorage",
+                          properties.getProperty("hosts"),
+                          properties.getProperty("bucket"),
+                          properties.getProperty("password"),
+                          properties.getProperty("batchSize")
+                    );
+        ExecJob job = pig.store("c", "output", storefunc.toString());
+
+        return job;
+    }
+
+    private FuncSpec function(String name, String ... args) {
+        FuncSpec fs = new FuncSpec(name, args);
+        if (log.isDebugEnabled()) log.debug("function: " + fs);
+
+        return fs;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // TODO instantiate and launch
+    }
+}
+
+// vim: set nospell:
---
+++ src/test/java/com/b5m/pig/UserCategoriesOnPig.java
@@ -0,0 +1,104 @@
+package com.b5m.pig;
+
+import com.b5m.utils.DatesGenerator;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.couchbase.client.CouchbaseClient;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.pig.backend.executionengine.ExecJob;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class UserCategoriesOnPig {
+
+    private final static DatesGenerator dategen = new DatesGenerator();
+
+    private final static String BUCKET = "default";
+    private final static String PASSWORD = "";
+    private final static String HOST = "http://127.0.0.1:8091/pools";
+
+    private static CouchbaseClient client;
+    private static List<Record> expectedOneDay = new ArrayList<Record>();
+    private static List<Record> expectedMultiDays = new ArrayList<Record>();
+
+    @BeforeClass
+    public static void connect() throws Exception {
+        List<URI> hosts = Arrays.asList(new URI(HOST));
+        client = new CouchbaseClient(hosts, BUCKET, PASSWORD);
+    }
+
+    @AfterClass
+    public static void shutdown() {
+        client.shutdown(1, TimeUnit.SECONDS);
+    }
+
+    @BeforeClass
+    private static void getExpectedOneDay() throws Exception {
+        List<String> lines = FileUtils.readLines(new File("src/test/data/user_categories_one.output"));
+        for (String line : lines) {
+            String text = appendDate(line, "2014-01-21");
+            expectedOneDay.add(Record.fromPig(text));
+        }
+    }
+
+    @BeforeClass
+    private static void getExpectedMultiDays() throws Exception {
+        List<String> lines = FileUtils.readLines(new File("src/test/data/user_categories_multi.output"));
+        for (String line : lines) {
+            String text = appendDate(line, "2014-01-21");
+            expectedMultiDays.add(Record.fromPig(text));
+        }
+    }
+
+    private static String appendDate(String line, String date) {
+        int i = line.indexOf(',');
+        StringBuilder sb = new StringBuilder(line.substring(0, i));
+        sb.append("::").append(date);
+        sb.append(line.substring(i));
+        return sb.toString();
+    }
+
+    @Test
+    public void oneDay() throws Exception {
+        UserCategories job = new UserCategories("2014-01-21");
+        job.addPropertiesFile("src/test/pig/user_categories.properties");
+
+        ExecJob results = job.run();
+        check(results);
+
+        for (Record record : expectedOneDay) check(record);
+    }
+
+    @Test
+    public void multiDays() throws Exception {
+        UserCategories job = new UserCategories("2014-01-21", 3); // three days
+        job.addPropertiesFile("src/test/pig/user_categories.properties");
+
+        ExecJob results = job.run();
+        check(results);
+
+        for (Record record : expectedMultiDays) check(record);
+    }
+
+    private void check(ExecJob result) {
+        assertEquals(result.getStatus(), ExecJob.JOB_STATUS.COMPLETED);
+    }
+
+    private void check(Record record) throws Exception {
+        String key = record.getUuid();
+        assertNotNull(client.get(key));
+        assertTrue(client.delete(key).get());
+    }
+
+}
+
